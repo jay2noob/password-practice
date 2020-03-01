1ee489b96fdfea5b06239c5bc60e894c035840d0
John Fields <jjfields24@outlook.com>
2020-02-29 23:33:34 -0500

    working

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
 
 const app = express();
 
@@ -10,10 +13,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.send("Welcome to Passport with Sequelize without Handlebars");
-});
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function() {
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("==> 🌍 Listening on port %s.  Visit http://localhost:%s/ in your browser.", PORT, PORT);
+   });
 });
